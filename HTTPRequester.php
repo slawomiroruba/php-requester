d0c3f2189a41b9dcc0fbc925c26db1ef9938024a
CyberPanel <support@cyberpanel.net>
2023-02-03 22:30:26 +0100

    composer.json and php file with class

---
+++ HTTPRequester.php
@@ -0,0 +1,74 @@
+<?php 
+
+class HTTPRequester {
+    /**
+     * @description Make HTTP-GET call
+     * @param       $url
+     * @param       array $params
+     * @return      HTTP-Response body or an empty string if the request fails or is empty
+     */
+    public static function HTTPGet($url, array $params = []) {
+        $query = http_build_query($params);  
+        $ch    = curl_init($params ? $url.'?'.$query : $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $response = json_decode(curl_exec($ch),true);
+        curl_close($ch);
+        return $response;
+    }
+    /**
+     * @description Make HTTP-POST call
+     * @param       $url
+     * @param       array $params
+     * @return      HTTP-Response body or an empty string if the request fails or is empty
+     */
+    public static function HTTPPost(string $url, array $params = [], array $headers = [], array $post_data = []) {
+        $final_url = $params ? $url.'?'. http_build_query($params) : $url;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $final_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
+        $response = json_decode(curl_exec($ch),true);
+        curl_close($ch);
+        return $response;
+    }
+    /**
+     * @description Make HTTP-PUT call
+     * @param       $url
+     * @param       array $params
+     * @return      HTTP-Response body or an empty string if the request fails or is empty
+     */
+    public static function HTTPPut($url, array $params) {
+        $query = \http_build_query($params);
+        $ch    = \curl_init();
+        \curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
+        \curl_setopt($ch, \CURLOPT_HEADER, false);
+        \curl_setopt($ch, \CURLOPT_URL, $url);
+        \curl_setopt($ch, \CURLOPT_CUSTOMREQUEST, 'PUT');
+        \curl_setopt($ch, \CURLOPT_POSTFIELDS, $query);
+        $response = \curl_exec($ch);
+        \curl_close($ch);
+        return $response;
+    }
+    /**
+     * @category Make HTTP-DELETE call
+     * @param    $url
+     * @param    array $params
+     * @return   HTTP-Response body or an empty string if the request fails or is empty
+     */
+    public static function HTTPDelete($url, array $params) {
+        $query = \http_build_query($params);
+        $ch    = \curl_init();
+        \curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
+        \curl_setopt($ch, \CURLOPT_HEADER, false);
+        \curl_setopt($ch, \CURLOPT_URL, $url);
+        \curl_setopt($ch, \CURLOPT_CUSTOMREQUEST, 'DELETE');
+        \curl_setopt($ch, \CURLOPT_POSTFIELDS, $query);
+        $response = \curl_exec($ch);
+        \curl_close($ch);
+        return $response;
+    }
+}
